0db21735d118e33a3c00103ece2acc6110431752
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-07-06 11:55:06 -0400

    aula 7

---
+++ Aulas/aula07/OperadoresPt1.js
@@ -0,0 +1,28 @@
+/*
+Operados Aritiméticos
+
++ soma
+- subtração
+* multiplicação
+/ divisão
+% divisão com resto
+** exponencição
+
+*/
+
+//Precedência
+var a = 5 + 3;
+var b = a % 5;
+var c = 5 * b ** 2;
+var d = 10 - a/2;
+var e = 6 * 2 / d;
+var f = b % e + 4 / e;
+
+//Auto-atribuição
+var n = 3;
+n = n + 4; //n += 4;
+n = n - 5; //n -= 5;
+n = n * 4; //n *= 4;
+n = n / 2; //n /= 2;
+n = n ** 2; //n **= 2;
+n = n % 5;  //n %= 5;
