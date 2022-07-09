3b5e53a88078a17d6ca2e70c55293ddef2c315cc
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-07-09 13:39:51 -0400

    aula 9

---
+++ Aulas/aula09/IntroducaoDOM.js
@@ -0,0 +1,11 @@
+//Document Object Model possui 5 formas diferentes para chamada de objetos, lembrando que tais objetos são referentes as partes de HTML e CSS do código e reproduzidos no JavaScript, são eles:
+
+//Por marca: getElementsByTagName()
+
+//Por Id: getElementsById()
+
+//Por nome: getElementsByName()
+
+//Por classe: getElementsByClassName()
+
+//Por seletor: querySelector() -> recentemente introduzida no JavaScript
