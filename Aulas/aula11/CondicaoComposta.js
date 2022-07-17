d7f6dc79946b55d3196d8461d9ae8dcf8fe83fb9
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-07-17 13:34:39 -0400

    aula 11

---
+++ Aulas/aula11/CondicaoComposta.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil';
+
+console.log('Seu país de origem é ' + pais);
+
+if(pais == 'Brasil'){
+    console.log('Você não é estrangeiro!!')
+} else{
+    console.log('Você é estrangeiro!!')
+}
---
+++ Aulas/aula11/CondicaoSimples.js
@@ -0,0 +1,7 @@
+var vel = 15;
+
+console.log('A velocidade do seu carro é de ' + vel + ' Km/h')
+
+if(vel >= 60){
+    console.log('Sua velocidade é maior que a permitida, está multado!')
+}
